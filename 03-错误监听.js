c40dbc42fb4db7c52d89266a752d914b13b39de3
youngs <lixiang@popicorns.com>
2024-12-02 18:10:29 +0800

    feat:错误处理中间件

---
+++ 03-错误监听.js
@@ -0,0 +1,56 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+    console.log('1');
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log('6');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    console.log('2');
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+    console.log('5');
+});
+
+// response
+
+app.use(async (ctx, next) => {
+    console.log('3');
+    ctx.body = 'Hello World';
+    console.log('4');
+    await next();
+});
+// 错误中间件
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (err) {
+        console.log('错误信息----', err);
+        ctx.status = err.statusCode || err.status || 500;
+        ctx.type = 'json';
+        ctx.body = {
+            ok: 0,
+            message: err.message
+        };
+        // 手动释放错误
+        ctx.app.emit('error', err, ctx);
+    }
+});
+app.use(async (ctx, next) => {
+    throw new Error('未知错误error');
+});
+app.on('error', (err, ctx) => {
+    console.log('错误信息', err);
+});
+app.listen(3003);
