afeb926f0b82458b53c182b87f52240958a15d80
youngs <lixiang@popicorns.com>
2024-12-02 17:33:22 +0800

    feat:中间件机制

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const Koa = require('koa');
 const app = new Koa();
 
-// response
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
+// logger
+
+app.use(async (ctx, next) => {
+    console.log('1');
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log('6');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+
 });
 
-app.listen(3002);
+// x-response-time
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    console.log('2');
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+    console.log('5');
+});
+
+// response
+
+app.use(async ctx => {
+    console.log('3');
+    ctx.body = 'Hello World';
+    console.log('4');
+});
+
+app.listen(3003);
